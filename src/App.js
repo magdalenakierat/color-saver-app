c379a4b0595fa4acf00e7a197ad283f03ea11ad2
Magdalena Kierat <m.kierat@gmx.de>
2022-06-27 19:30:07 +0200

    add state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
 import "./App.css";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import { ColorBox } from "./components/ColorBox";
 import { ColorForm } from "./components/ColorForm";
 
-const colors = [
-  {
-    id: 1,
-    code: "#ff0000",
-  },
-  {
-    id: 2,
-    code: "#00ff00",
-  },
-  {
-    id: 3,
-    code: "#0000ff",
-  },
-];
-
 function App() {
+  const [colors, setColors] = useState([]);
+
+  const handleAddColor = (code) => {
+    setColors([{ id: nanoid(), code }, ...colors]);
+  };
+
   return (
     <div className="App">
       <h1>My Color Saver App</h1>
-      <ColorForm />
+      <ColorForm onSubmit={handleAddColor} />
       <div className="card-grid">
         {colors.map((color) => {
           return <ColorBox color={color.code} key={color.id} />;
--- src/components/ColorForm.jsx
+++ src/components/ColorForm.jsx
@@ -1,9 +1,18 @@
 import "./ColorForm.css";
-export const ColorForm = () => {
+import { useState } from "react";
+export const ColorForm = ({ onSubmit }) => {
+  const [code, setCode] = useState("#000000");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(code);
+  };
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
   return (
-    <form className="color-form">
-      <input type="color" />
-      <input type="text" />
+    <form className="color-form" onSubmit={handleSubmit}>
+      <input type="color" value={code} onChange={handleChange} />
+      <input type="text" value={code} onChange={handleChange} />
       <button>Add</button>
     </form>
   );
