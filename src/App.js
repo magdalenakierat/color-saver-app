4bdfdef368cd5a703954be3781a34063c1203b02
Magdalena Kierat <m.kierat@gmx.de>
2022-06-27 12:58:49 +0200

    add color array

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import "./App.css";
 import { ColorBox } from "./components/ColorBox";
 
+const colors = [
+  {
+    id: 1,
+    code: "#ff0000",
+  },
+  {
+    id: 2,
+    code: "#00ff00",
+  },
+  {
+    id: 3,
+    code: "#0000ff",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <h1>My Color Saver App</h1>
       <div className="card-grid">
-        <ColorBox color="#f00" />
-        <ColorBox color="#0f0" />
-        <ColorBox color="#00f" />
+        {colors.map((color) => {
+          return <ColorBox color={color.code} key={color.id} />;
+        })}
       </div>
     </div>
   );
